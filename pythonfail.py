a5b2fdf0046be188b3c92447c00ab527e48151b6
Hugo Rodríguez <A00354911@itesm.mx>
2021-03-01 23:35:59 -0600

    Created py files

---
+++ pythonfail.py
@@ -0,0 +1,16 @@
+""" Lab 2.1 Python Development Exercises
+You need to create two functions to substitute str() and int(). A function called int_to_str() that converts integers
+into strings and a function called str_to_int() that converts strings into integers.
+
+Examples:
+int_to_str(4) ➞ "4"
+str_to_int("4") ➞ 4
+int_to_str(29348) ➞ "29348"
+
+:authors: - Hugo Rodríguez
+"""
+
+"""
+Test Cases:
+
+"""
